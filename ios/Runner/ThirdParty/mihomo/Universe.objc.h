d10fc2021bbb7b295471a42d602e2f87758eb17a
huangguodi <huangguodi@users.noreply.github.com>
2026-04-01 05:23:38 +0800

    fix(ios): restore gomobile support header definitions

--- ios/Runner/ThirdParty/mihomo/Universe.objc.h
+++ ios/Runner/ThirdParty/mihomo/Universe.objc.h
@@ -1,4 +1,16 @@
-#ifndef GO_UNIVERSE_OBJC_H
-#define GO_UNIVERSE_OBJC_H
+#ifndef __Universe_H__
+#define __Universe_H__
 @import Foundation;
+#include "ref.h"
+@protocol Universeerror;
+@class Universeerror;
+@protocol Universeerror <NSObject>
+- (NSString* _Nonnull)error;
+@end
+@interface Universeerror : NSError <goSeqRefInterface, Universeerror> {
+}
+@property(strong, readonly) _Nonnull GoSeqRef* _ref;
+- (nonnull instancetype)initWithRef:(_Nonnull GoSeqRef*)ref;
+- (NSString* _Nonnull)error;
+@end
 #endif
--- ios/Runner/ThirdParty/mihomo/ref.h
+++ ios/Runner/ThirdParty/mihomo/ref.h
@@ -1,6 +1,13 @@
-#ifndef GO_SEQ_REF_H
-#define GO_SEQ_REF_H
-@import Foundation;
-@protocol goSeqRefInterface <NSObject>
+#ifndef __GO_REF_HDR__
+#define __GO_REF_HDR__
+#include <Foundation/Foundation.h>
+@interface GoSeqRef : NSObject {}
+@property(readonly) int32_t refnum;
+@property(strong) id obj;
+- (instancetype)initWithRefnum:(int32_t)refnum obj:(id)obj;
+- (int32_t)incNum;
+@end
+@protocol goSeqRefInterface
+- (GoSeqRef*) _ref;
 @end
 #endif
